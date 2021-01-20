4d02dfda1e93967a307eee1c6726c1fb389abfec
Abhishek Rai <raigon48@gmail.com>
2021-01-20 13:57:09 +0530

    complete hometask2

---
+++ homeTask2/arithematicOp.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/basic-mathematical-operations
+
+function basicOp(operation, value1, value2) {
+    switch (operation) {
+      case "+":
+        return value1 + value2;
+        break;
+      case "-":
+        return value1 - value2;
+        break;
+      case "*":
+        return value1 * value2;
+        break;
+      case "/":
+        return value1 / value2;
+        break;
+    }
+  }
---
+++ homeTask2/transportOnVacation.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/transportation-on-vacation
+
+function rentalCarCost(d) {
+    if(d>6)
+      return (d*40)-50;
+    if(d>2)
+      return (d*40)-20;
+    return d*40;
+  }

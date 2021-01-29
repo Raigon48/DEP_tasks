5a71b449339925f467b3892d7ccfe727c5e3cea8
Abhishek Rai <raigon48@gmail.com>
2021-01-29 12:40:59 +0530

    complete homeTask2.3

---
+++ homeTask2.3/functionInFunction.js
@@ -0,0 +1,15 @@
+/*
+    https://www.codewars.com/kata/a-function-within-a-function
+
+    Given an input n, write a function always that returns a function which returns n. Ruby should return a lambda or a proc.
+
+    var three = always(3);
+    three(); // returns 3
+
+*/
+
+function always (num) {
+    return function () {
+      return num;
+    };
+  }
